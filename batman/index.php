79a2d0cba602262cf1da7818a9200cb55192e99f
skashyap777 <112409001+skashyap777@users.noreply.github.com>
2022-08-29 22:08:26 +0530

    Add files via upload

---
+++ batman/index.php
@@ -0,0 +1,47 @@
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Personle Website</title>
+    <link rel="stylesheet" href="styles.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+      integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+  </head>
+  <body>
+    <div class="hero">
+      <nav>
+        <img src="images/Batlo.png" class="logo" />
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">ABOUT</a></li>
+          <li><a href="#">BATMOBILE</a></li>
+          <li><a href="#">SERVICES</a></li>
+          <li><a href="#">GOTHAM</a></li>
+        </ul>
+      </nav>
+      <div class="detel">
+        <h1>I'm<span>BATMAN</span></h1>
+        <p>
+          This is my official website to showcase my all works related to GOTHAM
+        </p>
+        <a href="#">To the Batcave</a>
+      </div>
+      <div class="images">
+        <img src="images/Pic2.png" class="shape" />
+        <img src="images/bat.png" class="girl" />
+      </div>
+
+      <div class="social">
+        <a href="#"><i class="fab fa-dribbble"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-behance"></i></a>
+      </div>
+    </div>
+  </body>
+</html>
